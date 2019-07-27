a04308d3549520703969655b0c6f5b36ed008852
TiuYuri <yuri.o.franco@gmail.com>
2019-07-27 20:00:08 -0300

    deu merda

---
+++ bot.js
@@ -0,0 +1,39 @@
+var Discord = require('discord.js');
+var auth = require('./auth.json');
+var bot = new Discord.Client();
+var isReady = true;
+
+bot.on("ready", () => {
+    console.log("I am ready!");
+  });
+
+bot.on("message", (message) => {
+    if (isReady && message.content.startsWith(".")) {
+
+        isReady = false;
+        var voiceChannel = message.member.voiceChannel;
+
+        switch(message.content){
+            case '.poneilandia': 
+            voiceChannel.join().then(connection =>{
+                const dispatcher = connection.playFile('./audios/poneilandia.mp3');
+                dispatcher.on("end", end => {
+                    voiceChannel.leave();
+                    isReady = true;
+                });
+            }).catch(err => console.log(err));
+            break;
+            case '.sacanagem': 
+            voiceChannel.join().then(connection =>{
+                const dispatcher = connection.playFile('./audios/sacanagem_comigo.mp3');
+                dispatcher.on("end", end => {
+                    voiceChannel.leave();
+                    isReady = true;
+                });
+            }).catch(err => console.log(err));
+            break;
+        }
+    }
+});
+
+bot.login(auth.token);
